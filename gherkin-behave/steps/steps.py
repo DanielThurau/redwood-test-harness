447a3cc8a84cf8a9699e78bec81b539209d8fd60
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-07-05 21:29:18 -0700

    fixed inner sub-module

---
+++ gherkin-behave/steps/steps.py
@@ -0,0 +1,361 @@
+from behave import *
+from recordtype import recordtype
+import sys
+import random
+import os
+# parsert type
+use_step_matcher("parse")
+
+# ---------------------------------------
+# Changin this value to 'True' will have 
+# each step Dump what it did into whatever 
+# file is denoted in {file}
+DUMP = False;
+file = "test.txt"
+# ----------------------------------------
+
+# Gloabal default Parameters for state of experiemnt
+global_default_params = {}
+
+# User default paramters for users inside experiment
+user_default_params = {}
+
+# Record of price jumps w/ time at index [i][0] and value at [i][1]
+pricejumps = [[]]
+
+# Struct defining the users attributes 
+myStruct = {'subject': '', 'type': '', 'status': '', 'speed':'', 'bid0':'', 'offer0':''}
+
+# List containing each subject's struct
+subjects_list = []
+
+# list of scenario
+scenario_index = 0;
+
+# Array that mirrors subject_list 
+participant_type = []
+# num of subejcts
+subjects_num = 0
+# session_length
+session_length = 0
+#time records
+record_time = {}
+# CSV target
+target = open(file, 'w')
+
+#!--------------------------------------------------------------------->
+# Below this line is production code and does not need further modifications
+# or commenting
+#!--------------------------------------------------------------------->
+
+# Global default table parser
+@given('global default parameters')
+def step_impl(context):
+	# Grab from the table and add to global_default_params
+	for row in context.table:
+		global_default_params[row['parameter']] = row['value']
+
+	if(DUMP):
+		target.write("---------------Reading Global Parameters----------------\n")
+		for i in global_default_params:
+			target.write(i +' : ' + global_default_params[i] + '\n')
+
+# Reads in the global value of # of participants
+@given('there are {num} participants')
+def step_impl(context,num):
+	global subjects_num
+	subjects_num = int(num);
+	
+	global participant_type
+	participant_type = [None] * subjects_num
+
+	if(DUMP):
+		target.write("----------------Reading Number of Users----------------\n")
+		target.write("number of participants: " + num + '\n')
+
+# Reads in default values for participants
+@given('default parameters')
+def step_impl(context):
+	for row in context.table:
+			user_default_params[row['parameter']] = row['value']
+
+	if(DUMP):
+		target.write("----------Reading in User Default Parameters----------\n")
+		for i in user_default_params:
+			target.write(i +' : ' + user_default_params[i] + '\n')
+
+# vanialla python function to format print a Participant and its attrivutes
+def dump_subject(subject_dict):
+	target.write("Subject:  " + str(subject_dict['subject']) + '\n')
+	target.write("	Status: " + subject_dict['status'] + '\n')
+	target.write("	Speed:  " + subject_dict['speed'] + '\n')
+	target.write("	Bid0:   " + subject_dict['bid0'] + '\n')
+	target.write("	Offer0:	" + subject_dict['offer0'] + '\n')
+	target.write("	Type:	" + subject_dict['type'] + '\n')
+
+
+# Updates the length of the session
+@given('session lasts {t} seconds')
+def step_impl(context,t):
+	global session_length
+	session_length = t
+	if(DUMP):
+		target.write("----------------Updating Session Length----------------\n")
+		target.write("Sessions lasts: " + session_length + ' seconds\n')
+
+# Creates number of participants of participants using default participant values 
+@given('default parameters for users')
+def step_impl(context):
+	
+	global scenario_index
+	scenario_index = scenario_index + 1;
+	# Create direvtory
+	dir_create = "mkdir Scenario" + str(scenario_index)
+	os.system(dir_create)
+
+	if(scenario_index > 1):
+		reset_users();
+
+	 # Create a "struct" for a user and load the default values 
+	# into it. 
+	for i in range(0, subjects_num):
+
+		temp = dict(myStruct)
+
+		temp['subject'] = i
+		temp['type'] = "null"
+		temp['status'] = user_default_params['status']
+		temp['speed'] = user_default_params['speed']
+		temp['bid0'] = user_default_params['bid0']
+		temp['offer0'] = user_default_params['offer0']
+
+		if(DUMP):
+			target.write("----------------Creating Participant----------------\n")
+			dump_subject(temp)
+
+		# add it subject_list 
+		# !! subject list is the list that contains all users !!
+		subjects_list.append(temp)
+
+
+# Takes modified attributes of participants
+@given('participant <{p}> has {key} updated to {value}')
+def step_impl(context, p, key, value):
+	
+	# Get index of subject list 
+	subj_i = subj_index(p)
+
+	# if dne add it
+	if(subj_i == -1):
+		global subjects_num
+		subj_i = random.randint(0, subjects_num-1)
+		while subjects_list[subj_i] == "null":
+			subj_i = random.randint(0, subjects_num-1)
+		subjects_list[subj_i]['type'] = p;
+		# adds to mirror list to make searching concievable
+		participant_type[subj_i] = p;
+		if(DUMP):
+			target.write("----------------------Updating Type----------------------\n")
+			dump_subject(subjects_list[subj_i])
+
+	# update the value
+	subjects_list[subj_i][key] = value
+
+	if(DUMP):
+		target.write("-------------------Updating Attribute-------------------\n")
+		dump_subject(subjects_list[subj_i])
+
+# Takes intital V jump and creates the jump.csv for the experiemnt
+@when('V jumps to {value} at {time}')
+def step_impl(context, value, time):
+	global target
+	if(DUMP):
+		target.write("---------------Adding values to jump.csv---------------\n")
+
+	global scenario_index
+	jump_name = "Scenario" + str(scenario_index) + "/T"+ str(scenario_index) + "_jump.csv"
+	jump_targ = open(jump_name, 'w')
+
+	# initial header for the file
+	jump_targ.write("jumpTime,jumpSizes\n")
+	# V0
+	jump_targ.write("0," + global_default_params["V0"] +  "\n")
+	# trigger time
+	jump_targ.write(global_default_params[time] + ',' + value + "\n")
+
+	jump_targ.close();
+
+
+
+# I dont know why eric included this but its in it for now
+# Temporary
+@when('session runs to {time}')
+def step_impl(context, time):
+	global session_length
+	if(time == "completion"):
+		pass
+	else:
+		session_length = time;
+
+@when('an investor arrives to {d} at {t}')
+def step_impl(context, t, d):
+	time = parse_times_str(t)
+	target.write(str(time) + "\n")
+
+	# global target
+	if(DUMP):
+		target.write("---------------Adding values to investor.csv---------------\n")
+
+	global scenario_index
+	inv_name = "Scenario" + str(scenario_index) + "/T"+ str(scenario_index) + "_investors.csv"
+	inv_targ = open(inv_name, 'w')
+
+	# initial header for the file
+	inv_targ.write("investorTimes,investorDirections\n")
+
+	direction = -1
+
+	if(d == "buy"):
+		direction = 1
+	else:
+		direction = 0
+
+	inv_targ.write(str(time) + "," + str(direction))
+
+	inv_targ.close();
+
+
+# Vanialla python function to reset n numebr of 
+# participants to their default values
+def reset_users():
+	global participant_type
+	participant_type = [None] * subjects_num
+	for i in range(0, subjects_num):
+		subjects_list[i]['subject'] = i
+		subjects_list[i]['type'] = "null"
+		subjects_list[i]['status'] = user_default_params['status']
+		subjects_list[i]['speed'] = user_default_params['speed']
+		subjects_list[i]['bid0'] = user_default_params['bid0']
+		subjects_list[i]['offer0'] = user_default_params['offer0']
+
+		if(DUMP):
+			target.write("---------------Resetting Participants---------------\n")
+			dump_subject(subjects_list[i])
+		record_time = {}
+
+
+# Function that returns the index of the 
+# type_list
+def subj_index(type_label):
+	try:
+		n = participant_type.index(type_label)
+	except ValueError : 
+		return -1;
+	return n
+
+
+# Function that takes equation and substitues global 
+# values and evaluates
+def parse_times(dictionary):
+	# super ugly, just dont look at it please
+	for key in dictionary:
+		target.write(key + " : " + dictionary[key] + "\n")
+		temp = dictionary[key]
+		replaced_var = ""
+		tempword = ""
+		i = 0;
+		while i != len(temp):
+			if(temp[i] != "+" and temp[i] != "-" and temp[i] != "*" and temp[i] != "/"):
+				tempword = tempword + temp[i]
+				# target.write(tempword + "\n")
+			else:
+				replaced_var = replaced_var + global_default_params[tempword] + temp[i]
+				tempword = ""
+			if(i == len(temp)-1):
+				replaced_var = replaced_var + global_default_params[tempword]
+			i= i + 1 
+		dictionary[key] = eval(replaced_var)
+
+
+# Function that takes equation and substitues global 
+# values and evaluates
+def parse_times_str(time):
+	# super ugly, just dont look at it please
+	temp = time
+	replaced_var = ""
+	tempword = ""
+	i = 0;
+	while i != len(temp):
+		if(temp[i] != "+" and temp[i] != "-" and temp[i] != "*" and temp[i] != "/"):
+			tempword = tempword + temp[i]
+			# target.write(tempword + "\n")
+		else:
+			replaced_var = replaced_var + global_default_params[tempword] + temp[i]
+			tempword = ""
+		if(i == len(temp)-1):
+			replaced_var = replaced_var + global_default_params[tempword]
+		i= i + 1 
+	return eval(replaced_var)
+
+#!--------------------------------------------------------------------->
+# Above this line is production code and does not need further modifications
+# or commenting
+#!--------------------------------------------------------------------->
+
+
+
+
+
+
+
+# Times when the market is to be recorded
+# Parses and sets actual time
+@when('market state is recorded at')
+def step_impl(context):
+
+	# Gets number of columns by header
+	n = context.table.headings
+	global record_time
+	record_time = {}
+	# Add string represneting the calculation to its key
+	for row in context.table:
+		for value in n:
+			record_time[str(value)] = row[value]
+
+	# replace the string statments with
+	parse_times(record_time)
+
+	if(DUMP):
+		target.write("----------------Updating Record Times----------------\n")
+		for key in record_time:
+			target.write(key + " : " + str(record_time[key]) +"\n")
+
+@then('at {t} system is in initial state except V is {v}')
+def step_impl(context, t, v):
+	pass
+
+
+@then('at {t} participant <{p}> has {k} <{v}>')
+def step_impl(context, t, p, k, v):
+
+	subj_i = subj_index(p)
+
+
+	target_name = "Scenario" + str(scenario_index) + "/T" + str(scenario_index) + "_P" + str(subj_i) + "_input.csv"
+	user_target = open(target_name, 'a')
+
+	if( k == "bid"):
+		bid = subjects_list[subj_i]["bid0"]
+		offer = subjects_list[subj_i]["offer0"]
+		spread = int(offer) - int(bid)
+		user_target.write(str(record_time[t]) + ",SPREAD," + str(spread) + "\n")
+
+
+
+@then('at {t} participants besides <{p}> have {k} {v}')
+def step_impl(context, t, p, k, v):
+	pass
+
+@then('at {t} all participants have {k} {v}')
+def step_impl(context, t, k, v):
+	pass
---
+++ selenium/selenium_steps.py
@@ -0,0 +1,202 @@
+import time
+import unittest
+import sys
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+
+this_experiment = "HFT-CDA"
+manifest_loc = "/home/daniel/Documents/Programming/LEEPS/Code/2.2/redwood/static/redwood-high-frequency-trading-remote/manifest.json"
+config_loc = "/home/daniel/Documents/Programming/LEEPS/Code/2.2/redwood/static/experiments/redwood-high-frequency-trading-remote/config/test_config.csv"
+# this_experiment = sys.argv[1]
+# manifest_loc = sys.argv[2]
+# config_loc = sys.argv[3]
+time_bloc =  sys.argv[4]
+
+
+class local():
+	def setUp(self):
+		self.driver = webdriver.Chrome();
+		# Get Admin page and assert on what we received
+		self.driver.get("http://54.149.235.92/admin/")
+		self.nav("Log in | Django site admin","404")
+
+	# Login into https://54.149.235.92/admin using 
+	# credentials admin/password
+	def test_local(self):
+		self.login();
+		self.enter_experiment()
+
+
+	def login(self):
+		# Performs login for driver
+		elem_user = self.driver.find_element_by_name("username")
+		elem_user.clear()
+		elem_user.send_keys("admin")
+
+		elem_pass = self.driver.find_element_by_name("password")
+		elem_pass.clear()
+		elem_pass.send_keys("admin")
+		
+		#launch 
+		elem_pass.send_keys(Keys.RETURN)
+		self.nav("Site administration | Django site admin", "Failed to log in")
+
+	
+
+
+	# Define experiment (create new / access alresdy created one)
+	def enter_experiment(self):
+		# Navigate to the experiment tab
+		self.driver.get("http://54.149.235.92/admin/expecon/experiment/")
+		self.nav("Select experiment to change | Django site admin", "Did not reach experiments")
+		exists = True
+
+		# Try to grab all already entered experiements, and check to see if exists
+		try:
+			# Grab rows, stringify them , parse them by newline
+			rows = self.driver.find_element_by_id("result_list")
+			rows_string = rows.text
+			rows_string_parsed = rows_string.splitlines()
+			# Check if one is equal to 
+			for i in rows_string_parsed:
+				#*****************************************************************************
+				# if i == experiment: # Change to gloabal var
+				if i == this_experiment:
+				#*****************************************************************************
+					exists = False
+					elem = self.driver.find_element_by_link_text(this_experiment)
+					self.driver.get(elem.get_attribute("href"))
+					self.perform_experiment()
+		# Possibly a none existent element
+		except NoSuchElementException:
+			pass
+		if(exists):
+			# if DNE add the experiment
+	  		self.driver.get("http://54.149.235.92/admin/expecon/experiment/add/")
+			self.nav("Add experiment | Django site admin", "Failed to add experiment")
+
+			manifest_upload = self.driver.find_element_by_name("file")
+			#*******************************************************************************************
+			# Change file location
+			manifest_upload.send_keys(manifest_loc)
+
+			# manifest_upload.send_keys("/home/daniel/Documents/Programming/LEEPS/Code/2.2/redwood/static/redwood-high-frequency-trading-remote/manifest.json")
+			#*******************************************************************************************	
+
+			# Grab save & continue, then return
+			save = self.driver.find_element_by_name("_continue")
+			save.send_keys(Keys.RETURN)
+
+			# Find correct js script and execute to add new session
+			aElements = self.driver.find_elements_by_tag_name("a")
+			for name in aElements:
+			    if(name.get_attribute("href") is not None and "javascript:void(0)" in name.get_attribute("href") and "Add another Session" in name.text):
+			        name.click()
+			        break
+
+			# Grab save & continue, then return
+			save = self.driver.find_element_by_name("_continue")
+			save.send_keys(Keys.RETURN)
+
+			# Rename
+			experiment_name = self.driver.find_element_by_name("name")
+			experiment_name.clear()
+			#***************************************************************************
+			experiment_name.send_keys(this_experiment) # Change to gloabal variable
+			#***************************************************************************
+
+			# Grab save & continue, then return
+			save = self.driver.find_element_by_name("_continue")
+			save.send_keys(Keys.RETURN)
+
+			self.perform_experiment()
+
+	def perform_experiment(self):
+		# 
+		aElements = self.driver.find_elements_by_tag_name("a")
+		for name in aElements:
+		    if("View on site" in name.text):
+		    	session_text = name.get_attribute("href")
+		        i = len(session_text)-2
+		        session_number = ""
+		        while session_text[i] != '/':
+		        	session_number = session_number + str(session_text[i])
+		        	i = i - 1
+		        session_number = session_number[::-1]
+		        break
+
+
+		for i in range(1,5):
+			script = "window.open('http://54.149.235.92/session/"+session_number+"/subject/" + str(i) + "');"
+			self.driver.execute_script(script)
+			self.driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+			# time.sleep(2)
+			self.driver.switch_to_window(self.driver.window_handles[0])
+
+		for i in range(1,5):
+			self.driver.switch_to_window(self.driver.window_handles[i])
+			# time.sleep(4)
+		self.driver.switch_to_window(self.driver.window_handles[0])
+
+		
+
+
+		get_string = "http://54.149.235.92/session/"+session_number+"/admin"
+		self.driver.get(get_string)
+
+		# self.nav("Testing HFT Admin", "Failed to reach admin")
+		# time.sleep(10)
+		print("B4 FIRST RESET")
+		reset = self.driver.find_element_by_id("reset-session")
+		reset.send_keys(Keys.RETURN)
+		print("AFTER FIRST RESET")
+		# time.sleep(3)
+		config = self.driver.find_element_by_xpath("//*[@id=\"ui\"]/div[2]/div/input")
+		# iElements
+
+		start = self.driver.find_element_by_id("start-session")
+		start.send_keys(Keys.RETURN)
+
+		config.send_keys(config_loc)
+
+		# config.send_keys("/home/daniel/Documents/Programming/LEEPS/Code/2.2/redwood/static/experiments/redwood-high-frequency-trading-remote/config/test_config.csv")
+
+		time.sleep(float(time_bloc))
+		
+		download = self.driver.find_element_by_id("download")
+		download.send_keys(Keys.RETURN)
+
+	def nav(self, title, err):
+		assert title in self.driver.title, err
+
+	
+	def tearDown(self):
+		print("In destructor\n")
+		self.driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL+Keys.SHIFT+"q")
+		self.driver.close()
+
+
+
+
+
+x = local()
+x.setUp()
+x.test_local()
+# time.sleep(360)
+x.tearDown()
+
+		
+
+	
+
+
+
+# if __name__ == "__main__":
+
+# 	this_experiment = str(sys.argv[1])
+# 	manifest_loc = str(sys.argv[2])
+# 	config_loc = str(sys.argv[3])
+# 	sys.exit(unittest.main())
