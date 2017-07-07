4d4d6b84dca5e7354fdabd6faa80d6b78a1c8d6e
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-07-06 20:50:21 -0700

    driver updated

--- driver.py
+++ driver.py
@@ -7,9 +7,14 @@ file = sys.argv[1];
 osStatement = "cp " + file + " ./features/gherkin.feature"
 os.system(osStatement);
 
-# Dtiver Phase 2 : 
+# Driver Phase 2 : 
 # 	Run steps.py in the Behave Gherkin-behave module
 osStatement = "./features/steps/run.sh"
 os.system(osStatement); 
 osStatement = "rm -rf features/gherkin.feature"
+os.system(osStatement);
+
+# Driver Phase 3 :
+#	run python script on Scenarios dir
+osStatement = "python createConfig.py"
 os.system(osStatement);
