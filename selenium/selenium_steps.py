720c29b1dbca3c9a348a9b6c31703906c8dcf5ea
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-08-06 19:25:46 -0700

    kill browser after experminet

--- selenium/selenium_steps.py
+++ selenium/selenium_steps.py
@@ -177,7 +177,7 @@ class local():
 	def tearDown(self):
 		print("In destructor\n")
 		self.driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL+Keys.SHIFT+"q")
-		self.driver.quit()
+		# self.driver.quit()
         self.driver.close()
 
 
