e1789c6bff8087daa98b687597b6cd281825c554
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-10-14 16:36:09 -0700

    Helpe me

--- behave_driver.py
+++ behave_driver.py
@@ -1,12 +1,28 @@
 import fnmatch
 import os
-from Upload import *
 
 data_dir = "/home/dthurau/redwood-test-harness/data/scenarios/"
+experiment_dir = "/var/www/redwood/static/experiments/redwood-high-frequency-trading-remote/data/"
+experiment = "redwood-high-frequency-trading-remote/data/"
+prepend = "/static/experiments/" + experiment
 
-os.system("rm -rf data/scenarios/*")
 
-os.system("./features/steps/run.sh")
+
+def moveTo(file, dest):
+	scenario_dir = str(file.split('/')[-2]) + '/'
+
+	if not os.path.isdir(dest + scenario_dir):
+		os.system("mkdir " + str(dest + scenario_dir))
+
+	os.system("cp " + str(file) + " " + str(dest + scenario_dir))
+	return scenario_dir + file.split('/')[-1]
+
+
+
+
+#os.system("rm -rf data/scenarios/*")
+
+#os.system("./features/steps/run.sh")
 
 mydirs = []
 
@@ -17,12 +33,13 @@ for subdir, dirs, files in os.walk(data_dir):
 
 for diry in mydirs:
 	folder = "/" + os.path.basename(os.path.normpath(diry)) + "/"
-	investLink = uploadToDropbox([diry + 'T1_investors.csv',], folder)
-	jumpLink = uploadToDropbox([diry + 'T1_jump.csv'], folder)
+	investLink = [prepend + moveTo(diry + 'T1_investors.csv', experiment_dir)]
+	jumpLink = [prepend + moveTo(diry + 'T1_jump.csv', experiment_dir)]
 	inputLinks = []
-	for file in os.listdir(diry):
+	for file in sorted(os.listdir(diry)):
 		if fnmatch.fnmatch(file, '*_input.csv'):
-			inputLinks.append(uploadToDropbox([diry+file],folder))
+			inputLinks.append(prepend + moveTo(diry+file,experiment_dir))
+
 	with open(diry + '.configRead', 'w') as f:
 		for link in investLink:
 			f.write("invest>" + link + "\n")
@@ -30,5 +47,8 @@ for diry in mydirs:
 			f.write("jump>" + link + "\n")
 		count = 1;
 		for link in inputLinks:
-			f.write("input_" + str(count) + ">" + link[0] + "\n")
+			f.write("input_" + str(count) + ">" + link + "\n")
 			count = count + 1
+
+
+
