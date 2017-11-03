80f0a226cb9134e69218d7a049542c528c92b4ba
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2017-11-03 13:15:42 -0700

    back to dropbox days

--- behave_driver.py
+++ behave_driver.py
@@ -1,28 +1,34 @@
 import fnmatch
 import os
-
-data_dir = "/home/dthurau/redwood-test-harness/data/scenarios/"
-experiment_dir = "/var/www/redwood/static/experiments/redwood-high-frequency-trading-remote/data/"
-experiment = "redwood-high-frequency-trading-remote/data/"
-prepend = "/static/experiments/" + experiment
+from Upload import *
 
 
+data_dir = "/home/leeps/redwood-test-harness/data/scenarios/"
+# experiment_dir = "/var/www/redwood/static/experiments/redwood-high-frequency-trading-remote/data/"
+# experiment = "redwood-high-frequency-trading-remote/data/"
+# prepend = "/static/experiments/" + experiment
 
-def moveTo(file, dest):
-	scenario_dir = str(file.split('/')[-2]) + '/'
 
-	if not os.path.isdir(dest + scenario_dir):
-		os.system("mkdir " + str(dest + scenario_dir))
 
-	os.system("cp " + str(file) + " " + str(dest + scenario_dir))
-	return scenario_dir + file.split('/')[-1]
+# def moveTo(file, dest):
+# 	scenario_dir = str(file.split('/')[-2]) + '/'
+
+# 	if not os.path.isdir(dest + scenario_dir):
+# 		os.system("mkdir " + str(dest + scenario_dir))
+
+# 	os.system("cp " + str(file) + " " + str(dest + scenario_dir))
+# 	return scenario_dir + file.split('/')[-1]
 
 
 
 
 #os.system("rm -rf data/scenarios/*")
+try:
+	os.remove('data/scenarios/*')
+except OSError:
+	pass
 
-#os.system("./features/steps/run.sh")
+os.system("./features/steps/run.sh")
 
 mydirs = []
 
@@ -33,13 +39,12 @@ for subdir, dirs, files in os.walk(data_dir):
 
 for diry in mydirs:
 	folder = "/" + os.path.basename(os.path.normpath(diry)) + "/"
-	investLink = [prepend + moveTo(diry + 'T1_investors.csv', experiment_dir)]
-	jumpLink = [prepend + moveTo(diry + 'T1_jump.csv', experiment_dir)]
+	investLink = uploadToDropbox([diry + 'T1_investors.csv',], folder)
+	jumpLink = uploadToDropbox([diry + 'T1_jump.csv'], folder)
 	inputLinks = []
-	for file in sorted(os.listdir(diry)):
+	for file in os.listdir(diry):
 		if fnmatch.fnmatch(file, '*_input.csv'):
-			inputLinks.append(prepend + moveTo(diry+file,experiment_dir))
-
+			inputLinks.append(uploadToDropbox([diry+file],folder))
 	with open(diry + '.configRead', 'w') as f:
 		for link in investLink:
 			f.write("invest>" + link + "\n")
@@ -47,8 +52,11 @@ for diry in mydirs:
 			f.write("jump>" + link + "\n")
 		count = 1;
 		for link in inputLinks:
-			f.write("input_" + str(count) + ">" + link + "\n")
+			f.write("input_" + str(count) + ">" + link[0] + "\n")
 			count = count + 1
-
-
-
+	print("INPUTS")
+	print(inputLinks)
+	print("JUMPS")
+	print(jumpLink)
+	print("INVESTS")
+	print(investLink)
--- driver.py
+++ driver.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python
-
 import os
 import sys
+import shutil
 
-# print("usage: python driver.py gherkin-file")
 
 TOPDIR = os.path.dirname(os.path.realpath(__file__)) + "/"
-MANIFEST= TOPDIR + "data/manifests/CDA_Manifest.json"
+MANIFEST = TOPDIR + "data/manifests/CDA_Manifest.json"
 TYPE = "CDA"
 
 # Driver Phase 1 :
 #	Copy Specified Gherkin text to Gherkin Module
-file = sys.argv[1];
-# print(file)
-osStatement = "cp " + file + " ./features/gherkin.feature"
-# print(osStatement)
-os.system(osStatement);
-
+try:
+	shutil.copyfile(sys.argv[1], "./features/gherkin.feature")
+except ParseError:
+	pass
 # Driver Phase 2 : 
 # 	Run steps.py in the Behave Gherkin-behave module
 osStatement = "python behave_driver.py"
 os.system(osStatement); 
-osStatement = "rm -rf features/gherkin.feature"
-os.system(osStatement);
+os.remove(TOPDIR + "features/gherkin.feature")
 
 # Driver Phase 3 :
 #	run python script on Scenarios dir
@@ -30,33 +26,33 @@ osStatement = "python createConfig.py"
 os.system(osStatement);
 
 
-# Driver Phase 4:
-#       trigger selenium
-scenarios = next(os.walk('data/scenarios'))[1]
-for directory in scenarios:
-	while True:
-		Join = raw_input('Would you like to run a selenium experiment?(y/n)\n')
-		if Join == "y" or Join == "yes" or Join == "Y" or Join == "yes":
-			break
-		elif Join == "n" or Join == "no" or Join == "N" or Join == "no":
-			exit()
-		else:
-			Join = raw_input('Would you like to run a selenium experiment?(y/n)\n')
-	osStatement = "selenium/selenium_driver " + TYPE + " " + MANIFEST + " " + TOPDIR + "data/scenarios/" +  directory + " 45"
-	# print(osStatement)
-	os.system(osStatement);
+# # Driver Phase 4:
+# #       trigger selenium
+# scenarios = next(os.walk('data/scenarios'))[1]
+# for directory in scenarios:
+# 	while True:
+# 		Join = raw_input('Would you like to run a selenium experiment?(y/n)\n')
+# 		if Join == "y" or Join == "yes" or Join == "Y" or Join == "yes":
+# 			break
+# 		elif Join == "n" or Join == "no" or Join == "N" or Join == "no":
+# 			exit()
+# 		else:
+# 			Join = raw_input('Would you like to run a selenium experiment?(y/n)\n')
+# 	osStatement = "selenium/selenium_driver " + TYPE + " " + MANIFEST + " " + TOPDIR + "data/scenarios/" +  directory + " 45"
+# 	# print(osStatement)
+# 	os.system(osStatement);
 
-	# Copy exchange server logs to current directory
-	copyStatemnt = "rsync -avz --remove-source-files -e ssh ubuntu@ec2-54-149-235-92.us-west-2.compute.amazonaws.com:/var/HFT2/CDA_DATA/* " + TOPDIR + "data/scenarios/" + directory
-	# print(copyStatemnt)
-	os.system(copyStatemnt)
+# 	# Copy exchange server logs to current directory
+# 	copyStatemnt = "rsync -avz --remove-source-files -e ssh ubuntu@ec2-54-149-235-92.us-west-2.compute.amazonaws.com:/var/HFT2/CDA_DATA/* " + TOPDIR + "data/scenarios/" + directory
+# 	# print(copyStatemnt)
+# 	os.system(copyStatemnt)
 
 
-# Driveer Phase 5:
-#       trigger query 
+# # Driveer Phase 5:
+# #       trigger query 
 
-scenarios = next(os.walk('data/scenarios'))[1]
-for directory in scenarios:
-	osStatement = "python query.py data/scenarios/" + directory + "/query.config data/scenarios/" + directory + "/output.csv"
-	# print(osStatement)
-	os.system(osStatement);
+# scenarios = next(os.walk('data/scenarios'))[1]
+# for directory in scenarios:
+# 	osStatement = "python query.py data/scenarios/" + directory + "/query.config data/scenarios/" + directory + "/output.csv"
+# 	# print(osStatement)
+# 	os.system(osStatement);
